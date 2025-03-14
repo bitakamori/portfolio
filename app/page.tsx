951605e8d942830b4f3765f923c3a0004f04541d
hmazzuchetti <biancahelenatakamoriribeiro@gmail.com>
2025-03-14 17:05:50 -0300

    adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/app/components/post";
+import Link from "next/link";
 import RepeatIcon from "@mui/icons-material/Repeat";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
@@ -9,8 +10,8 @@ export default function Bianca() {
         <Post
           src="/eu.jpg"
           alt="Bianca Takamori"
-          width={100}
-          height={100}
+          width={80}
+          height={80}
           isPost={true}
         >
           <div className="flex justify-between items-center">
@@ -20,7 +21,7 @@ export default function Bianca() {
               <FavoriteIcon className="text-red-900 ml-3 mr-2" />
             </div>
           </div>
-          <div className="px-3">
+          <div>
             <p className="text-zinc-900 text-lg mt-2">Quem sou eu</p>
             <p className="text-zinc-500 text-md mt-2">
               Oi! Meu nome é Bianca Takamori e sou desenvolvedora frontend. Sou
@@ -32,9 +33,13 @@ export default function Bianca() {
               JavaScript, TypeScript, Vue.js, React.js, Sass, Bootstrap, Vuetify
               e Tailwind. <br /> Sou movida a desafios e adoro resolver
               problemas. Quando não estou programando, costumo ficar com meus
-              gatos, crochetando, assistindo séries e documentários ou lendo.
+              gatos, crochetando, assistindo séries e documentários ou lendo.{" "}
+              <br />
             </p>
-            <p className="text-gray-400 text-sm my-3">
+            <Link href={"/info"} className="text-[#21334d] hover:text-gray-400">
+              Sobre o projeto
+            </Link>
+            <p className="text-gray-400 text-sm mt-3">
               #maedepet #potterhead #bookLovers #crochetLovers
             </p>
           </div>
